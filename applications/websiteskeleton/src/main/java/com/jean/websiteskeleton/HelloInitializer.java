9f8321e23836aac0edd92357bedbd6538d8db4ab
Jean de Klerk <jadekler@gmail.com>
2015-04-07 11:17:51 -0600

    Renamed pingpongpi to websiteskeleton

--- applications/websiteskeleton/src/main/java/com/jean/websiteskeleton/HelloInitializer.java
+++ applications/websiteskeleton/src/main/java/com/jean/websiteskeleton/HelloInitializer.java
@@ -1,4 +1,4 @@
-package com.jean.pingpongpi;
+package com.jean.websiteskeleton;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- applications/websiteskeleton/src/main/java/com/jean/websiteskeleton/WebMvcConfiguration.java
+++ applications/websiteskeleton/src/main/java/com/jean/websiteskeleton/WebMvcConfiguration.java
@@ -1,4 +1,4 @@
-package com.jean.pingpongpi;
+package com.jean.websiteskeleton;
 
 import org.springframework.context.annotation.*;
 import org.springframework.web.servlet.ViewResolver;
