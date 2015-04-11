64bd7e68544685c9eee6188d46f92bdf81a8e4a2
Jean de Klerk <jadekler@gmail.com>
2015-04-11 15:21:30 -0600

    Products controller test

---
+++ components/products/src/test/java/products/ProductsControllerTest.java
@@ -0,0 +1,35 @@
+package products;
+
+import com.websiteskeleton.products.ProductsController;
+import org.junit.*;
+import org.mockito.InjectMocks;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+public class ProductsControllerTest {
+    @InjectMocks ProductsController controller;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() throws Exception {
+        controller = new ProductsController();
+        mockMvc = standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void testGetUsers() throws Exception {
+        String sampleJson = "{[" +
+            "{\"name\":\"Super Glue\"}," +
+            "{\"name\":\"Kool-Aide\"}" +
+            "]}";
+
+        mockMvc.perform(get("/products"))
+            .andExpect(status().isOk())
+            .andExpect(content().string(sampleJson))
+            .andExpect(header().string("content-type", "application/json"));
+    }
+}
