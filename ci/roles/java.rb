a29ae00950c56e2e45b68e71f4371a6003a5c96a
Jean de Klerk <jadekler@gmail.com>
2015-04-20 20:50:01 -0600

    Some cleanup

--- ci/roles/java.rb
+++ ci/roles/java.rb
@@ -1,14 +1,14 @@
-name "java"
-description "Install Oracle Java"
+name 'java'
+description 'Install Oracle Java'
 default_attributes(
-    "java" => {
-        "install_flavor" => "oracle",
-        "jdk_version" => "8",
-        "oracle" => {
-            "accept_oracle_download_terms" => true
+    'java' => {
+        'install_flavor' => 'oracle',
+        'jdk_version' => '8',
+        'oracle' => {
+            'accept_oracle_download_terms' => true
         }
     }
 )
 run_list(
-    "recipe[java]"
+    'recipe[java]'
 )
