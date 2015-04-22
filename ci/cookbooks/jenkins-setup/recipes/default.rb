498b324df1a3dba6a322e4f7bfd1f980d938e3f6
Jean de Klerk <jadekler@gmail.com>
2015-04-22 08:10:08 -0600

    Added jenkins job, forwarded port

--- ci/cookbooks/jenkins-setup/recipes/default.rb
+++ ci/cookbooks/jenkins-setup/recipes/default.rb
@@ -1,10 +1,13 @@
 include_recipe 'jenkins::master'
 
+jenkins_plugin 'git'
+jenkins_plugin 'gradle'
+
 skeleton_build = File.join(Chef::Config[:file_cache_path], 'skeleton-build-job.xml')
 
 template skeleton_build do
     source 'skeleton-build-job.xml.erb'
-    variables :git_repo => 'git@github.com:jadekler/git-spring-websiteskeleton.git'
+    variables :git_repo => 'https://github.com/jadekler/git-spring-websiteskeleton.git'
 end
 
 jenkins_job 'skeleton_build' do
