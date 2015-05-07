e4506d9b8b94c0a05cbb2887c37c0575ce09beeb
Jean de Klerk <jadekler@gmail.com>
2015-05-07 12:22:55 -0600

    Clean up imports

--- components/products/src/main/java/com/websiteskeleton/products/ProductsController.java
+++ components/products/src/main/java/com/websiteskeleton/products/ProductsController.java
@@ -1,7 +1,5 @@
 package com.websiteskeleton.products;
 
-import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
--- components/users/src/main/java/com.websiteskeleton.users/UsersController.java
+++ components/users/src/main/java/com.websiteskeleton.users/UsersController.java
@@ -2,7 +2,6 @@ package com.websiteskeleton.users;
 
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
