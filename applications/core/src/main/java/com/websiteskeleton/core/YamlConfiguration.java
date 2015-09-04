4f23d13183fa8e56fc9387c24ae3fa06df2d556d
Jean de Klerk <jadekler@gmail.com>
2015-09-03 19:58:02 -0600

    Start trying to get yaml configuration working

---
+++ applications/core/src/main/java/com/websiteskeleton/core/YamlConfiguration.java
@@ -0,0 +1,19 @@
+package com.websiteskeleton.core;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class YamlConfiguration {
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+        yaml.setResources(new ClassPathResource("application.yml"));
+        propertySourcesPlaceholderConfigurer.setProperties(yaml.getObject());
+
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
--- components/products/src/main/java/com/websiteskeleton/products/ProductsController.java
+++ components/products/src/main/java/com/websiteskeleton/products/ProductsController.java
@@ -2,6 +2,8 @@ package com.websiteskeleton.products;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.PropertySources;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,13 @@ import static java.util.Arrays.asList;
 @Controller
 public class ProductsController {
     private final ObjectMapper jsonMapper = new ObjectMapper();
+
+    @Autowired PropertySources propertySources;
+
+    public ProductsController() {
+        System.out.println(propertySources);
+        System.exit(0);
+    }
 
     @RequestMapping(value = "/products", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
